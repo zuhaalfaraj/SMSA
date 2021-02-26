faf0450aba3b124106de18a311657849395641ca
zuha <zuha.alfaraj@gmail.com>
2021-02-26 22:27:00 +0300

    re-organize the code

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from YahooHistoricalData import YahooFinanceHistory
+from DailyStockMarket import StockMarket
+from SentimentAnalysis import NewsSentiment
 
+class StockMarketSentimentAnalysis():
+    def __init__(self,symbol):
+        self.daily_market = StockMarket(symbol)
+        self.historical_data = YahooFinanceHistory(symbol)
+        self.news_sentiment = NewsSentiment(symbol)
 
-
+if __name__ =='__main__':
+    pass
