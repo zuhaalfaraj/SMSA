a96845c2610c1515c0bef5aaf3fce82008ef2bb2
zuha <zuha.alfaraj@gmail.com>
2021-03-04 00:13:04 +0300

    update style

--- app.py
+++ app.py
@@ -10,19 +10,23 @@ import pandas as pd
 import plotly.express as px
 from main import StockMarketSentimentAnalysis
 import requests
+from sklearn.preprocessing import StandardScaler
+from dateutil.relativedelta import relativedelta
+
 app = Flask(__name__)
 
 def plot_sentiment(ticker):
     global smsa
     smsa = StockMarketSentimentAnalysis(ticker)
     sentiment_data = smsa.get_sentiment()
-    mean_df = sentiment_data.groupby('date').mean()
+    mean_df = sentiment_data.groupby('date').mean()[:-1]
     mean_df= mean_df.reset_index()
     data = [
         go.Bar(
             name='sentiment index',
             x= mean_df['date'],
-            y=mean_df['sentiment']
+            y=mean_df['sentiment'],
+        marker = {'color': 'Gold'}
         )
     ]
 
@@ -35,13 +39,58 @@ def plot_h_date():
         go.Bar( name='price difference',
             x= h_data['Date'][1:],
             y=h_data['Close'].diff()[1:],
-            marker={'color': 'red'}
+            marker={'color': 'DarkSeaGreen'}
         )
     ]
 
     graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
     return graphJSON
+def plot_price_vs_sent():
+    h_data= smsa.get_related_historical_data()
+    h_data_std = StandardScaler().fit_transform(h_data[['Close', 'Open']].diff()[1:])
+    sentiment_data = smsa.get_sentiment()
+    sentiment_data = sentiment_data.groupby('date').mean()
+    sentiment_data= sentiment_data.reset_index()
 
+    data_1 = [
+        go.Bar(
+            name='std historical change',
+            x=h_data['Date'][1:],
+            y= pd.Series(h_data_std[:,0]),
+            marker={'color': 'DarkSeaGreen'}
+        )
+    ]
+    data_2 = [
+        go.Bar(
+            name='sentiment index',
+            x= sentiment_data['date'],
+            y=sentiment_data['sentiment'],
+            marker={'color': 'Gold'}
+
+        )
+    ]
+    graphJSON_1 = json.dumps(data_1, cls=plotly.utils.PlotlyJSONEncoder)
+    graphJSON_2 = json.dumps(data_2, cls=plotly.utils.PlotlyJSONEncoder)
+    return graphJSON_1, graphJSON_2
+
+
+
+def plot_pie():
+    values= smsa.get_sentiment_cat()
+    labels= ['positive news', 'negative news', 'natural']
+    colors = ['LightSalmon', 'LightCoral', 'LightBlue']
+
+    data= [go.Pie(labels=labels, values=values, marker=dict(colors=colors))]
+    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+    return graphJSON
+
+def plot_pie_change():
+    values= smsa.get_perc_change()
+    labels= ['<-25%','-25% to -1%','0% to 25%','25% to 50%','50%-100%', '>100%']
+    colors = ['PeachPuff', 'RosyBrown', 'Moccasin', 'Linen', 'LemonChiffon', 'Lavender']
+    data= [go.Pie(labels=labels, values=values, marker=dict(colors=colors))]
+    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+    return graphJSON
 
 @app.route('/', methods=['POST'])
 def my_form_post():
@@ -57,8 +106,12 @@ def my_form_post():
 
     bar = plot_sentiment(text)
     bar_2 = plot_h_date()
+    pie_sent= plot_pie()
+    pie_change=plot_pie_change()
+    hst, sent= plot_price_vs_sent()
 
-    return render_template('index.html', ticker= text, plot=bar,plot_2= bar_2,
+    return render_template('index.html', ticker= text, plot=bar,plot_2= bar_2,pie_sent=pie_sent,
+                           pie_change=pie_change,hst=hst,sent=sent,
                            price=price, change=change, market_cap=market_cap,
                            open_price=open_price, range=range)
 
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from YahooHistoricalData import YahooFinanceHistory
 from DailyStockMarket import StockMarket
 from SentimentAnalysis import NewsSentiment
-
+import datetime
 class StockMarketSentimentAnalysis:
     def __init__(self,symbol):
         self.daily_market = StockMarket(symbol)
@@ -10,22 +10,42 @@ class StockMarketSentimentAnalysis:
 
     def get_pre_data(self):
         return self.daily_market.get_data()
+
     def get_sentiment(self):
         data = self.news_sentiment.get_news_data()
         date = data['date'].unique()
         self.start_date = date[-1]
-        self.end_date = date[0]
+        self.end_date = datetime.datetime.today()
         return data
 
     def get_related_historical_data(self):
         self.hestorical_data = self.daily_market.get_historical_data(self.start_date.year, self.start_date.month,
                                               self.start_date.day, self.end_date.year,
                                               self.end_date.month, self.end_date.day)
+
         return self.hestorical_data
 
     def get_historical_data(self,start_year,start_month,start_day,end_year,end_month,end_day):
         return self.daily_market.get_historical_data(start_year,start_month,
                                                      start_day,end_year,end_month,end_day)
+
+    def get_sentiment_cat(self):
+        cat = self.news_sentiment.get_cat()
+        return [len(cat[0]), len(cat[1]), len(cat[2])]
+
+    def get_perc_change(self):
+        x = self.daily_market.get_one_year_history()
+        x['change'] = x['Close'].apply(lambda i: i / x['Close'].loc[0] * 100 - 100)
+
+        drop_25 = len(x[x['change']<0][x['change']>=-25])
+        drop_more=len(x['change'][x['change']>-25])
+        to_25 = len(x['change'][x['change']>0][x['change']<=25])
+        to_50= len(x['change'][x['change']>25][x['change']<=50])
+        more_50 = len(x['change'][x['change']>50][x['change']<=100] )
+        more_100= len(x['change'][x['change']>100])
+
+        return [drop_more,drop_25,to_25,to_50,more_50,more_100]
+
 
 
 if __name__ =='__main__':
